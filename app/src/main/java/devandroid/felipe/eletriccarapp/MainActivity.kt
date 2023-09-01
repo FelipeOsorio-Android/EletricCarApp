d7475e7a8a19230a2ffee44f22f14769d8d69b63
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2023-09-01 00:07:49 -0300

    Criação da primeira tela do App

--- app/src/main/java/devandroid/felipe/eletriccarapp/MainActivity.kt
+++ app/src/main/java/devandroid/felipe/eletriccarapp/MainActivity.kt
@@ -2,10 +2,15 @@ package devandroid.felipe.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import devandroid.felipe.eletriccarapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
